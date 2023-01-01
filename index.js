c1f31c8b7a6af4e0bc2827fa9e46a24c078007e2
vvsshukla <vvsshukla@gmail.com>
2023-01-01 13:14:47 +0530

    input from command line

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let a=10;
-let b=0;
-setTimeout(()=>{
-	b=20;
-}, 2000);
-console.log(a+b);
+const fs = require('fs');
+const input = process.argv;
+if(input[2]=='add')
+{
+	fs.writeFileSync(input[3], input[4]);
+}
+else if(input[2]=='remove')
+{
+	fs.unlinkSync(input[3]);
+}
+else
+{
+	console.log('invalid input');
+}
